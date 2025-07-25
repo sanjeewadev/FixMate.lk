57b8c0d822bc208527943fb6362ba662b283ee4b
sanjeewa-dev <svk6061@gmail.com>
2025-07-25 22:55:27 +0530

    clean main

--- server/src/routes/coordinatorRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { registerCoordinator, loginCoordinator } = require("../controllers/coordinatorController");
-
-const router = express.Router();
-
-router.post("/register", registerCoordinator);
-router.post("/login", loginCoordinator);
-
-module.exports = router;
